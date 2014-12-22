96b1998c1eda206557afc0d1cc39fff228414941
franckie20 <franck.verschuur@gmail.com>
2014-12-22 16:58:55 +0100

    test
    test

--- src/Pattern.java
+++ src/Pattern.java
@@ -9,5 +9,52 @@ public class Pattern {
 	private String solution;
 	private Image diagram;
 	private String consequence;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getContext() {
+		return context;
+	}
+	
+	public void setContext(String context) {
+		this.context = context;
+	}
+	
+	public String getProblem() {
+		return problem;
+	}
+	
+	public void setProblem(String problem) {
+		this.problem = problem;
+	}
+	
+	public String getSolution() {
+		return solution;
+	}
+	
+	public void setSolution(String solution) {
+		this.solution = solution;
+	}
+	
+	public Image getDiagram() {
+		return diagram;
+	}
+	
+	public void setDiagram(Image diagram) {
+		this.diagram = diagram;
+	}
+	
+	public String getConsequence() {
+		return consequence;
+	}
+	
+	public void setConsequence(String consequence) {
+		this.consequence = consequence;
+	}
 
 }
