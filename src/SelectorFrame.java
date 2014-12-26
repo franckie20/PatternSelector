d5634974407d99926ea4e584264afdf5a010d62c
franckie20 <franck.verschuur@gmail.com>
2014-12-26 13:15:53 +0100

    pff
    pff

--- src/SelectorFrame.java
+++ src/SelectorFrame.java
@@ -15,31 +15,30 @@ public class SelectorFrame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
-	//private JPanel contentPanel;
+	// private JPanel contentPanel;
 	@SuppressWarnings("unused")
-	private JLabel picture, l1,l2, l3, l4, l5, l6, l8;
+	private JLabel picture, l1, l2, l3, l4, l5, l6, l8;
 	private SelectorController control;
 	@SuppressWarnings({ "rawtypes", "unused" })
 	private JComboBox box1, box2, box3, box4, box5, box6;
 
-
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public SelectorFrame(SelectorController sc) {
 		super("Pattern Application");
 		control = sc;
-		
+
 		Font f = new Font("SansSerif", Font.BOLD, 12);
 		JPanel firstPanel = new JPanel();
 		firstPanel.setBorder(new TitledBorder("Pattern Selector"));
-		firstPanel.setLayout(new GridLayout(5,1,1,1));
-				
+		firstPanel.setLayout(new GridLayout(5, 1, 1, 1));
+
 		l1 = new JLabel("Context: ");
 		l1.setFont(f);
-		firstPanel.add(l1);		
-		box1 = new JComboBox();
-		
+		firstPanel.add(l1);
+		box1 = new JComboBox(control.getAllPatternContexts());
+
 		firstPanel.add(box1);
-		box1.addActionListener(this);
+		box1.addActionListener(contextAction);
 		l2 = new JLabel("Problem: ");
 		l2.setFont(f);
 		firstPanel.add(l2);
@@ -69,33 +68,27 @@ public class SelectorFrame extends JFrame implements ActionListener {
 		content.setBorder(new EmptyBorder(10, 10, 10, 10));
 		content.add(firstPanel, BorderLayout.NORTH);
 		add(content, BorderLayout.CENTER);
-		
+
 		setSize(560, 360);
 		setTitle("Pattern Selector");
 		setLocationRelativeTo(null);
-	
-		
 
 		onSelectedItemChanged();
 	}
-	
-		
-		
-	//	String[] listPatterns = control.getAllPatternsByName();
-	//	JComboBox<String> patternList = new JComboBox<String>(listPatterns);
 
-	//	patternList.setSelectedIndex(1);
-	//	patternList.addActionListener(contextAction);
-		//pn.add(patternList);
+	// String[] listPatterns = control.getAllPatternsByName();
+	// JComboBox<String> patternList = new JComboBox<String>(listPatterns);
 
-	//CONTEXT OPHALEN???
+	// patternList.setSelectedIndex(1);
+	// patternList.addActionListener(contextAction);
+	// pn.add(patternList);
+
+	// CONTEXT OPHALEN???
 	private void onSelectedItemChanged() {
-		Object obj = box1.getSelectedItem();
-		if (obj instanceof Pattern) {
-			Pattern p = (Pattern)obj;
-			p.getContext();
-		}
-		else {
+		Object context = box1.getSelectedItem();
+		if (context.equals(control.getAllPatterns())) {
+			System.out.println("Test");
+		} else {
 
 		}
 	}
@@ -103,92 +96,66 @@ public class SelectorFrame extends JFrame implements ActionListener {
 	public SelectorController getSelectorController() {
 		return control;
 	}
+
 	public void setControl(SelectorController sc) {
 		control = sc;
 	}
 
-	
 	ActionListener contextAction = new ActionListener() {
-		
-	@Override
-	public void actionPerformed(ActionEvent e) {
-	
-	}
-	};
 
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == box1) {
+		@Override
+		public void actionPerformed(ActionEvent e) {
 			onSelectedItemChanged();
 		}
-		
+	};
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
 	}
 }
 
-
-
-
 // Set up the picture.
-/*		picture = new JLabel();
-picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
-picture.setHorizontalAlignment(JLabel.CENTER);
-updateLabel(listPatterns[patternList.getSelectedIndex()]);
-picture.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+/*
+ * picture = new JLabel();
+ * picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
+ * picture.setHorizontalAlignment(JLabel.CENTER);
+ * updateLabel(listPatterns[patternList.getSelectedIndex()]);
+ * picture.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+ * 
+ * // The preferred size is hard-coded to be the width of the // widest image
+ * and the height of the tallest image + the border. // A real program would
+ * compute this. picture.setPreferredSize(new Dimension(315, 320 + 10));
+ * 
+ * add(l1, BorderLayout.BEFORE_LINE_BEGINS); //add(l2, BorderLayout.LINE_END);
+ * add(patternList, BorderLayout.PAGE_START); add(picture,
+ * BorderLayout.PAGE_END); picture.setBorder(BorderFactory.createEmptyBorder(20,
+ * 20, 20, 20)); }
+ */
 
-// The preferred size is hard-coded to be the width of the
-// widest image and the height of the tallest image + the border.
-// A real program would compute this.
-picture.setPreferredSize(new Dimension(315, 320 + 10));
-
-add(l1, BorderLayout.BEFORE_LINE_BEGINS);
-//add(l2, BorderLayout.LINE_END);
-add(patternList, BorderLayout.PAGE_START);
-add(picture, BorderLayout.PAGE_END);
-picture.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-}
-*/
-
-/*		@Override
-public void actionPerformed(ActionEvent e) {
-	@SuppressWarnings("rawtypes")
-	JComboBox cb = (JComboBox) e.getSource();
-	String patternName = (String)cb.getSelectedItem();
-	updateLabel(patternName);
-}
-};
-
-protected void updateLabel(String name) {
-ImageIcon icon = createImageIcon("images/" + name.toLowerCase()
-		+ ".gif");
-picture.setIcon(icon);
-picture.setToolTipText("The " + name.toLowerCase() + " pattern");
-if (icon != null) {
-	picture.setText(null);
-} else {
-	picture.setText("Image not found");
-}
-}
-
-
-/** Returns an ImageIcon, or null if the path was invalid. */
-/*	protected static ImageIcon createImageIcon(String path) {
-BufferedImage img = null;
-try {
-	img = ImageIO.read(new URL("http://www.franckie20.com/" + path));
-} catch (MalformedURLException e) {
-	e.printStackTrace();
-} catch (IOException e) {
-	e.printStackTrace();
-}
-
-if (img != null) {
-	return new ImageIcon(img);
-} else {
-	System.err.println("Couldn't find file: " + path);
-	return null;
-}
-}
-*/
-
+/*
+ * @Override public void actionPerformed(ActionEvent e) {
+ * 
+ * @SuppressWarnings("rawtypes") JComboBox cb = (JComboBox) e.getSource();
+ * String patternName = (String)cb.getSelectedItem(); updateLabel(patternName);
+ * } };
+ * 
+ * protected void updateLabel(String name) { ImageIcon icon =
+ * createImageIcon("images/" + name.toLowerCase() + ".gif");
+ * picture.setIcon(icon); picture.setToolTipText("The " + name.toLowerCase() +
+ * " pattern"); if (icon != null) { picture.setText(null); } else {
+ * picture.setText("Image not found"); } }
+ * 
+ * 
+ * /** Returns an ImageIcon, or null if the path was invalid.
+ */
+/*
+ * protected static ImageIcon createImageIcon(String path) { BufferedImage img =
+ * null; try { img = ImageIO.read(new URL("http://www.franckie20.com/" + path));
+ * } catch (MalformedURLException e) { e.printStackTrace(); } catch (IOException
+ * e) { e.printStackTrace(); }
+ * 
+ * if (img != null) { return new ImageIcon(img); } else {
+ * System.err.println("Couldn't find file: " + path); return null; } }
+ */
 
